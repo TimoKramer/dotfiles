6cb95ddce1c9d98d84fa4e01dad57e672aa7a3ad
Timo Kramer <fw-git@timokramer.de>
2022-11-16 13:45:31 +0100

    feat: wezterm shortcuts for autostart wsl

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -10,9 +10,41 @@ end
 local home = wt.home_dir
 local color_scheme_dirs = join(home, ".config", "wezterm", "colors")
 
+local keys = is_windows and {
+    {
+        key = 'T',
+        mods = 'CTRL|SHIFT',
+        action = wt.action.SpawnCommandInNewTab {
+            args = { 'wsl', '--cd', '~' },
+        },
+    },
+    {
+        key = 'N',
+        mods = 'CTRL|SHIFT',
+        action = wt.action.SpawnCommandInNewWindow {
+            args = { 'wsl', '--cd', '~' },
+        },
+    },
+    {
+        key = '%',
+        mods = 'CTRL|SHIFT|ALT',
+        action = wt.action.SplitHorizontal {
+            args = { 'wsl', '--cd', '~' },
+        },
+    },
+    {
+        key = '"',
+        mods = 'CTRL|SHIFT|ALT',
+        action = wt.action.SplitVertical {
+            args = { 'wsl', '--cd', '~' },
+        },
+    },
+} or {}
+
 return {
-	color_scheme_dirs = { color_scheme_dirs },
-	color_scheme = 'modus-operandi',
-    font_size = 11.0,
+    color_scheme_dirs = { color_scheme_dirs },
+    color_scheme = 'modus-operandi',
+    font_size = 9.0,
     hide_tab_bar_if_only_one_tab = true,
+    keys = keys,
 }
