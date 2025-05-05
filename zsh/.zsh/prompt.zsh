25bd85f4cee45200c6c457b00ee151fb42057dce
Timo Kramer <git@timokramer.de>
2025-05-05 09:19:40 +0200

    simplified git prompt

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -1,4 +1,21 @@
-# PURE
-fpath+=($HOME/.zsh/pure)
-autoload -U promptinit; promptinit
-prompt pure
+# Load vcs_info
+autoload -Uz vcs_info
+
+# Set up vcs_info
+zstyle ':vcs_info:*' enable git
+zstyle ':vcs_info:git:*' formats '%b %m%u%c'
+zstyle ':vcs_info:git:*' actionformats '%b|%a %m%u%c'
+
+# Enable checking for staged/unstaged changes
+zstyle ':vcs_info:git:*' check-for-changes true
+zstyle ':vcs_info:git:*' stagedstr '+'
+zstyle ':vcs_info:git:*' unstagedstr '*'
+
+# Run vcs_info before each prompt
+precmd() {
+  vcs_info
+}
+
+# Set the prompt to include vcs_info
+setopt prompt_subst
+PROMPT='%F{cyan}%~%f %F{green}${vcs_info_msg_0_}%f $ '
