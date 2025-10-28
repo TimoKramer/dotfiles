c163732a7131eec171e7884180cb74380fd8ecff
Timo Kramer <fw-git@timokramer.de>
2025-10-28 12:21:10 +0100

    some stuffystuff

--- nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
@@ -1,30 +1,55 @@
 return {
-  'miikanissi/modus-themes.nvim',
-  priority = 1000,
-  dim_inactive = 'non-current',
-  styles = {
-    comments = { sp = 'green' },
+  {
+    'miikanissi/modus-themes.nvim',
+    priority = 1000,
+    dim_inactive = 'non-current',
+    styles = {
+      comments = { sp = 'green' },
+    },
+    config = function()
+      vim.cmd [[set cursorcolumn]]
+      vim.cmd [[highlight CursorColumn guibg=#d0d6ec]]
+    end,
   },
-  config = function()
-    vim.cmd [[colorscheme modus_operandi]]
-    vim.cmd [[set cursorcolumn]]
-    vim.cmd [[highlight CursorColumn guibg=#d0d6ec]]
-  end,
-}
+  {
+    'p00f/alabaster.nvim',
+    config = function()
+      vim.cmd [[set background=light]]
+      vim.cmd [[colorscheme alabaster]]
+    end,
+  },
 
--- return {
---   'projekt0n/github-nvim-theme',
---   lazy = false, -- make sure we load this during startup if it is your main colorscheme
---   priority = 1000, -- make sure to load this before all the other start plugins
---   config = function()
---     require('github-theme').setup({
---       specs = {
---         all = {
---           syntax = {
---             keyword = 'orange',
---     }}}
---     })
---
---     vim.cmd('colorscheme github_light')
---   end,
--- }
+  { -- You can easily change to a different colorscheme.
+    -- Change the name of the colorscheme plugin below, and then
+    -- change the command in the config to whatever the name of that colorscheme is.
+    --
+    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+    'folke/tokyonight.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    init = function()
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme 'tokyonight-night'
+
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+  {
+    'projekt0n/github-nvim-theme',
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require('github-theme').setup {
+        specs = {
+          all = {
+            syntax = {
+              keyword = 'orange',
+            },
+          },
+        },
+      }
+    end,
+  },
+}
