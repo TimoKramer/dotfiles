88d1d877a5b3dc3db47015d0dd0705e345547e61
Timo Kramer <fw-git@timokramer.de>
2025-10-31 09:58:57 +0100

    suspend with lock

---
+++ niri/.config/niri/scripts/lid-close.sh
@@ -0,0 +1,3 @@
+#!/bin/env bash
+
+gtklock && systemctl suspend
--- niri/.config/niri/scripts/power_menu.sh
+++ niri/.config/niri/scripts/power_menu.sh
@@ -13,7 +13,7 @@ case $SELECTION in
         gtklock;;
     *"󰤄 Suspend"*)
         if confirm_action "Suspend"; then
-            systemctl suspend
+            gtklock && systemctl suspend
         fi;;
     *"󰍃 Log out"*)
         if confirm_action "Log out"; then
