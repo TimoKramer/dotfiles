4d382d259977707e8f7e5960ef62c08a4a44eb8c
Timo Kramer <fw-git@timokramer.de>
2025-11-14 15:30:24 +0100

    stuff

---
+++ niri/.config/niri/scripts/after-resume.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# triggered by udev rule /etc/udev/rules.d/95-monitor-hotplug.rules
+# https://wiki.archlinux.org/title/Udev#Execute_when_HDMI_cable_is_plugged_in_or_unplugged
+
+# Enable all outputs that exist
+niri msg -j outputs | jq -r 'keys[]' | while read -r output; do
+    echo "activating ${output}"
+    niri msg output "$output" on
+done
