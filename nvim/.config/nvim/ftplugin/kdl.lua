1e869c0eee84f8872479d21d221045c8135d258b
Timo Kramer <fw-git@timokramer.de>
2025-11-19 17:58:52 +0100

    kdl format

---
+++ nvim/.config/nvim/ftplugin/kdl.lua
@@ -0,0 +1,7 @@
+-- vim.bo.commentstring = '// %s'
+
+-- vim.b.undo_ftplugin = (vim.b.undo_ftplugin or '') .. '\n setl comments< commentstring< formatoptions<'
+vim.opt_local.shiftwidth = 4
+vim.opt_local.tabstop = 4
+vim.opt_local.softtabstop = 4
+vim.opt_local.expandtab = true
