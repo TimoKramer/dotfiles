ccd6c2e7643074c4c6289e156b41dc8daff72afe
Timo Kramer <fw-git@timokramer.de>
2025-11-21 11:54:32 +0100

    stuff

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -8,7 +8,7 @@ alias lls="eza --all --sort=name --long --group-directories-first --header --byt
 alias cat='bat'
 alias grep='rg --hidden --ignore-case --follow'
 alias gits='git status'
-alias gitc='git commit -S'
+alias gitc='git commit -S -m'
 alias gitd='git diff'
 alias gitl='git log --graph --oneline --pretty="format:%h %G? %aN  %s"'
 alias gitg='git grep'
